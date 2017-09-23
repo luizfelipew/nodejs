4365b3ea6b21f8e459bf49f686b75bc2dcf9757e
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2017-09-23 13:52:16 -0300

    Criancao da model e da rota

---
+++ models/StormtroppersModel.js
@@ -0,0 +1,46 @@
+'use strict';
+
+function StormtropperDAO(model) {
+  this.model = model;
+}
+
+StormtropperDAO.prototype.create = function (data, callback) {
+  var model = new this.model(data);
+  model.save(function(err, result) {
+    callback(err, result);
+  });
+};
+
+StormtropperDAO.prototype.find = function (data, callback) {
+  this.model.find(query).exec(callback);
+};
+
+StormtropperDAO.prototype.findOne = function (_id, data, callback) {
+  var query = {_id: _id };
+  this.model.findOne(query).exec(callback);
+};
+
+StormtropperDAO.prototype.update = function (_id, data, callback) {
+  var query = {_id: _id };
+  this.model.update(query, data).exec(function(err, result){
+    callback(err, result);
+  });
+};
+
+StormtropperDAO.prototype.remove = function (_id, data, callback) {
+  var query = {_id: _id };
+  this.model.remove(query, data).exec(function(err, result){
+    callback(err, result);
+  });
+};
+
+
+module.exports = function(mongoose){
+  var Stormtropper = mongoose.model('Stormtropper', {
+    name:       String,
+    nickname:   String,
+    divisions:  [String],
+    patent:     String
+  });
+  return new StormtropperDAO(Stormtropper);
+};
